54f2f7691b5d5824ac0a761515902fcf6cf9233f
Carlos Indriago <carlos.indriago@tsoftglobal.com>
2021-12-10 13:20:42 -0300

    Fixed logo margin top for mible

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -18,7 +18,7 @@ const Header: React.FC<IHeaderProps> = () => {
     <header className="h-auto w-full bg-[#ac171c] flex flex-col items-center justify-center md:flex-row md:p-8 md:items-start">
       <Link href="/" passHref>
         <div
-          className="flex items-center mt-7 cursor-pointer"
+          className="flex items-center mt-6 cursor-pointer"
           onClick={handleReload}
         >
           <h1 className="text-4xl font-permanent text-white uppercase">
